c4dce5757237442ca1c88e8ba8fafab65e6cd258
GCC <1112@>
2025-08-26 21:28:18 +0800

    shorten 2012

--- 2000-2099/2012/2012.py
+++ 2000-2099/2012/2012.py
@@ -1,11 +1,5 @@
-b = []
-n = 0
-a = list(map(int, input().split()))
+l = list(map(int, input().split()))
+dp = [1] * 10
 for i in range(10):
-    j = len([x for x in b if x < a[i]])
-    n = max(n, j + 1)
-    if j >= len(b):
-        b.append(a[i])
-    else:
-        b[j] = a[i]
-print(n)
+    dp[i] = max([dp[i]] + [dp[j] + 1 for j in range(i) if l[j] < l[i]])
+print(max(dp))
